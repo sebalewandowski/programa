13fb6e457081058da60af9205efa9a669ad8c07d
Sebastian Lewandowski <sebastianlewandowski@Sebastians-MacBook-Pro.local>
2019-03-18 13:26:29 +0100

    init commit for task-3

---
+++ assets/file.js
@@ -0,0 +1,26 @@
+function validateForm() {
+    var imie = document.forms["cv"]["imie"].value;
+    var nazwisko = document.forms["cv"]["nazwisko"].value;
+    var wiek = document.forms["cv"]["wiek"].value;
+    var plec = document.forms["cv"]["plec"].value;
+    var zgoda = document.forms["cv"]["zgoda"];
+
+    if (imie.length<1) {
+        document.getElementById('error-imie').innerHTML = " Please Enter Your Name *"
+    }
+    if (nazwisko.length<1) {
+        document.getElementById('error-nazwisko').innerHTML = " Please Enter Your Surname *";
+    }
+    if (wiek.length<1) {
+        document.getElementById('error-wiek').innerHTML = " Please Enter Your Age *";
+    }
+    if (plec.length<1) {
+        document.getElementById('error-plec').innerHTML = " Please Enter Your Sex *";
+    }
+    if (zgoda.checked == 0) {
+        document.getElementById('error-zgoda').innerHTML = " This checkbox is required *";
+    }
+    if(imie.length<1 || nazwisko.length<1 || wiek.length<1 || plec.length<1 || zgoda.checked == 0){
+        return false;
+    }
+}
